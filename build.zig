503f375b2405dd580457001428b180ff95f639c9
Hugo Machet <mail@hmachet.com>
2024-05-07 14:34:52 +0200

    build: addModule() use fcft instead of zig-fcft
    so we can do
        `const fcft = b.dependency("zig-fcft", .{}).module("fcft");`

--- build.zig
+++ build.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 const Build = std.Build;
 
 pub fn build(b: *Build) !void {
-    _ = b.addModule("zig-fcft", .{
+    _ = b.addModule("fcft", .{
         .root_source_file = .{ .path = "fcft.zig" },
     });
 }
