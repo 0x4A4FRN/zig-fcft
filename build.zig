7881eac29bf0d9de4df1fc31df380e8f5f620585
Hugo Machet <mail@hmachet.com>
2024-05-07 15:16:04 +0200

    build: Fix pixman dependency import

--- build.zig
+++ build.zig
@@ -2,7 +2,11 @@ const std = @import("std");
 const Build = std.Build;
 
 pub fn build(b: *Build) !void {
+    const pixman = b.dependency("zig-pixman", .{}).module("pixman");
     _ = b.addModule("fcft", .{
         .root_source_file = .{ .path = "fcft.zig" },
+        .imports = &.{
+            .{ .name = "pixman", .module = pixman },
+        },
     });
 }
