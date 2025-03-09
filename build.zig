b757bf4e430a4415f1755958c43afeb8373c25cf
LN Liberda <lauren@selfisekai.rocks>
2025-03-09 22:52:22 +0100

    Update to Zig 0.14

--- build.zig
+++ build.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 const Build = std.Build;
 
 pub fn build(b: *Build) !void {
-    const pixman = b.dependency("zig-pixman", .{}).module("pixman");
+    const pixman = b.dependency("pixman", .{}).module("pixman");
     _ = b.addModule("fcft", .{
         .root_source_file = b.path("fcft.zig"),
         .imports = &.{
